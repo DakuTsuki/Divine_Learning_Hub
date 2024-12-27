382291515d3b61fd09d5139742c570c9e77d863c
Dakutsuki <demomega9@gmail.com>
2024-12-27 07:19:37 +0100

    push

--- src/app/Components/Tracks/page.tsx
+++ src/app/Components/Tracks/page.tsx
@@ -48,9 +48,10 @@ export default function Tracks() {
               {/* Map five stars */}
               {Array(5)
               .fill(null)
-              .map(()=>(
-                <IoStarSharp className="w-5 h-5 text-yellow-500"/>
+              .map((_, index) => (
+                <IoStarSharp key={index} className="w-5 h-5 text-yellow-500" />
               ))}
+            
 
             </div>
 
